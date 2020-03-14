bd49401b4f749694b8685f4adc32728c3ea8c491
Deep <deep.cloud2208@gmail.com>
2020-03-14 22:44:26 +0530

    some modifications during training

--- Python_Day3_Functions.py
+++ Python_Day3_Functions.py
@@ -1,29 +1,37 @@
 #-------------------------------------------------------------------------------
 # Python Functions/Methods
-# Two types - i) user defined functions
-#            ii) built-in functions
+# Two types - i) user defined functions - which we define. we write the code
+#                                         in the function.
+#            ii) built-in functions - open(), list(), set(), tuple(), dict(),
+#                                     type(), str(), int(), min(), max(), avg()
+#
+# How to call a Function - <function-name>(arg) - func(a)
+#
 #-------------------------------------------------------------------------------
 # Basic Function
 #-------------------------------------------------------------------------------
 # def myFunc():
 #     print("Hello there. Welcome to Day 3. I am inside of function")
+#     print("this the second line inside of the function")
 #
 # myFunc()
 
+# def myFunc(a,b): # a=20, b=30
+#     c = a+b
+#     # return (c)
+#     return (a*b)
 
-# def myFunc(a,b):
-#     return (a+b)
-#     # return (a*b)
-#
-# var1 = myFunc(20,30)
+# print("hi there")
+# var1 = myFunc(20,30) # var1 would have the value after "return" statement, which is value of c
 # print(var1)
 
 #-------------------------------------------------------------------------------
 # Pass List/Tuple/Set as arguments
 #-------------------------------------------------------------------------------
 # def ArrayFunc(array):
-#     funcVar = array.pop()
+#     print("a has been captured in 'array' variable: ", array)
 #     # funcVar = array[0]
+#     funcVar = array.pop()
 #     return funcVar
 
 # a = list()
@@ -33,7 +41,7 @@
 # a.append('HDFS')
 # a.append('MapReduce')
 
-# a = [ 'Spark', 'Python', 'PySpark']
+# a = [ 'Spark', 'Python', 'PySpark', 'HDFS', 'MapReduce']
 
 # a = tuple()
 # a = ('Spark', 'Python', 'PySpark')
@@ -44,11 +52,12 @@
 # var1 = a
 # print("var1 (before passing to ArrayFunc): ",var1)
 #
+# print("a (outside of ArrayFunc): ", a)
 # var1 = ArrayFunc(a)
 # print("var1 (after coming from ArrayFunc): ",var1)
 #
 
-# def func_to_return_values(array):
+# def func_to_return_multiple_values(array):
     # var1 = array[0]
     # var2 = array[1]
     # var1_set = array.pop()
@@ -56,13 +65,13 @@
     # return var1, var2
     # return var1_set, var2_set
 
-# var = (1,2,3,4)
+# var = ("Deep","AWS", "GCP")
 # var = [1,2,3,4]
 # var = {1,2,3,4}
-# var_out = func_to_return_values(var)
-# print(var_out)
-# var_out1 = func_to_return_values(var)[0]
-# var_out1 = func_to_return_values(var)[1]
+# var_out = func_to_return_multiple_values(var)
+# print("Getting multiple values in 'return' statement: ", var_out)
+# var_out1 = func_to_return_multiple_values(var)[0]
+# var_out1 = func_to_return_multiple_values(var)[1]
 
 #-------------------------------------------------------------------------------
 # Pass Dictionary as argument
@@ -85,13 +94,17 @@
 #         'Programming2': 'Java'
 #     }
 # var['bussiness_use_case'] = 'Data Warehouse and Analytics'
-#
+
 # print("var (before passing to DictFunc): ",var)
-#
+
+# var_out1=DictFunc(var)
+# print(var_out1)
+
 # var_out1=DictFunc(var)[0]
 # var_out2=DictFunc(var)[1]
 # print("subject: ", var_out1)
 # print("optional: ", var_out2)
+# print(var_out2['Programming1'])
 
 #-------------------------------------------------------------------------------
 # Pass multiple arguments to a function
@@ -107,32 +120,39 @@
 #
 # myArray = list()
 # myArray = ['Maths', 'Chemistry', 'Physics', 'Biology']
-#
-# # MultiArgFunc(myDict,myArray)
+
+# MultiArgFunc(myDict,myArray)
 # print(MultiArgFunc(myDict,myArray))
 
 #-------------------------------------------------------------------------------
 # Call by Value & Call by Reference
 #-------------------------------------------------------------------------------
-# 1) Call by value is passing normal variables
+# 1) Call by value is passing normal variables like Integer, Decimal, String
 # 2) Call by Reference is passing List/Array. As the variable only contains
-#    pointer to the Array, changes to the items inside function does not impact
-#    does not impact it outside
+#    pointer to the Array, changes to the items inside function impacts it
+#    outside
 #-------------------------------------------------------------------------------
-# a = [10,20,30]
+# a1 = [10,20,30]
 # a = {10,20,30}
 # a = (10,20,30)
-# a = 10
-# def chageit(b):
-#     print("b: ", b)
-    # b = 20
-    # b[1] = 100
-    # b[2] = 1000
-    # print("b: ", b)
-
-# print("a: ", a)
-# chageit(a)
-# print("a: ", a)
+# a2 = 10
+# def func_call_by_value(b):
+#     print("b (before assignment): ", b)
+#     b = 20
+#     print("b (after assignment): ", b)
+#
+# def func_call_by_reference(b):
+#     print("b (before assignment): ", b)
+#     b[1] = 100
+#     b[2] = 1000
+#     print("b (after assignment): ", b)
+#
+#
+# func_call_by_value(a2)
+# print("a2 (after returning from func - call by value): ", a2)
+#
+# func_call_by_reference(a1)
+# print("a1 (after returning from func - call by reference): ", a1)
 
 #-------------------------------------------------------------------------------
 # Args and Kwargs - pass as many arguments as you want
@@ -141,16 +161,21 @@
 # def ArgKwargFunc(arg1,*args, **kwargs):
 #     print("arg1: ", arg1)
 #     print("args: ", args)
-#     print("kwargs", kwargs)
-#
+#     print("kwargs: ", kwargs)
+
 # mylist = ['a','b']
 # myset = {'set1', 'set2'}
 # mytuple = (1,2)
 # mydict1 = {"FName" : "Soumyadeep", "LName" : "Dey", "Exp": 16}
 # mydict2 = {"Tech": ['aws', 'gcp', 'big data']}
 
+# mylist = "mylist"
+# myset = "myset"
+# mytuple = 100
+
 # ArgKwargFunc(mylist,myset,mytuple,mydict1,mydict2,name="Deep",tech="Spark",exp=16,cloud=True)
 # ArgKwargFunc(mylist,myset,mytuple)
+# ArgKwargFunc(name="Intellipaat",subject="PySpark")
 # ArgKwargFunc(mylist,myset,mytuple,Name="Intellipaat", Type="Training Institute")
 
 # -------------------------------------------------------------------------------
@@ -158,14 +183,14 @@
 # Note: Not used that much
 # -------------------------------------------------------------------------------
 # def ScopeFunc():
-#     # global x
-#     # global y
-#     # print(x)
-#     x = "PySpark - LEGB"
-#     # y = "Y is Inside ScopeFunc"
-#     print("<INSIDE ScopeFunc, after assignment> value of x: ", x)
-#     # print("<INSIDE ScopeFunc, after assignment> value of y: ", y)
-#     print('  ')
+    # global x
+    # global y
+    # print(x)
+    # x = "PySpark - LEGB"
+    # y = "Y is Inside ScopeFunc"
+    # print("<INSIDE ScopeFunc, after assignment> value of x: ", x)
+    # print("<INSIDE ScopeFunc, after assignment> value of y: ", y)
+    # print('  ')
 
 # def ScopeFunc(y):
 #     print("x: ",x)
@@ -175,13 +200,13 @@
 #     print(y)
 #     print(x)
 
-
 # x = 'DeepSpark'
 #
 # print("'x' before passing to ScopeFunc: ", x)
 # print("'y' before passing to ScopeFunc: ", y)
 # print('  ')
 # ScopeFunc()
+# print("'x' after coming from ScopeFunc: ", x)
 # ScopeFunc(x)
 # print("'x' after coming from ScopeFunc (NOTICE THE VALUE, it did not change): ", x)
 # print("'y' after coming from ScopeFunc (NOTICE THE VALUE, it was changed in ScopeFunc): ", y)
